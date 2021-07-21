d7daaca6fdb582dc534ade8755c84d78d1aaf836
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-07-21 21:51:00 +0700

    module

--- index.js
+++ index.js
@@ -5,3 +5,19 @@ console.log("Mencampurkan air dan kopi");
 console.log("Menuangkan kopi ke dalam gelas");
 console.log("Menuangkan susu ke dalam gelas");
 console.log("Kopi Anda sudah siap!");
+
+// import coffeeStock from "state";
+// const coffeeStock = require("./state");
+const {coffeeStock, isCoffeeMachineReady} = require("./state");
+
+const makeCoffee = (type, miligrams) => {
+  if (coffeeStock[type] >= miligrams) {
+    console.log("Kopi berhasil dibuat!");
+  } else {
+    console.log("Biji kopi habis!");
+  }
+};
+
+makeCoffee("robusta", 80);
+console.log(coffeeStock);
+console.log(isCoffeeMachineReady);
